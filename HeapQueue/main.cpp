69e1a64ae5aa8abb9d8ed8a5bf32f18fe47c15c9
Dario Caric <dcaric@me.com>
2024-05-29 19:39:24 +0200

    Initial Commit

---
+++ HeapQueue/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HeapQueue
+//
+//  Created by Dario Caric on 29.05.2024..
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
